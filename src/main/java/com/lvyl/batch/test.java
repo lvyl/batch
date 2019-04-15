7e64c59dc524210829091629be521787cf978057
lvyl <15290806520@139.com>
2019-04-15 11:32:27 +0800

    batch

--- src/main/java/com/lvyl/batch/test.java
+++ src/main/java/com/lvyl/batch/test.java
@@ -1,9 +1,7 @@
-package com.lvyl.batch.utils;
+package com.lvyl.batch;
 
-import com.lvyl.batch.actions.GetLotteryTicketDataTask;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,15 +9,39 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
-@Component
-public class HttpUtil {
-    Logger logger = LoggerFactory.getLogger(GetLotteryTicketDataTask.class);
 
+/**
+ * @author yllv
+ * @datetime 2019-04-15 11:17
+ * @desc
+ */
+@SuppressWarnings("all")
+public class test {
     public static void main(String[] args) {
-        System.out.println(new HttpUtil().sendPost("http://www.lottery.gov.cn/api/lottery_kj_detail_new.jspx","_ltype=4&_term=19041"));
-    }
+        /**
+         * 获取最新一期的号码
+         */
+        int newTerm = 0;
+        String result = sendPost("http://www.lottery.gov.cn/api/lottery_kj_detail_new.jspx", "_ltype=4&_term=");
+        JSONObject jsonObjectEventName = new JSONObject(result.substring(1, result.length() - 1));
+        JSONArray eventNameList = jsonObjectEventName.getJSONArray("eventName");
+        newTerm = Integer.parseInt(eventNameList.get(0)+"");
+        /**
+         * 从最新一期循环到 07001
+         */
+        for(int term=newTerm;term>7001;term--){
+            String termStr = term<10001?"0"+term:""+term;
+            /**
+             * 判断这一期是否在开奖号码
+             */
+            String isExits = sendPost("http://www.lottery.gov.cn/api/lottery_kj_detail_new.jspx", "_ltype=4&_term="+termStr);
+            if(isExits.length()<20){
 
-    public String sendPost(String url, String param) {
+                break;
+            }
+        }
+    }
+    public static String sendPost(String url, String param) {
         PrintWriter out = null;
         BufferedReader in = null;
         String result = "";
@@ -40,7 +62,7 @@ public class HttpUtil {
             for(in = new BufferedReader(new InputStreamReader(conn.getInputStream())); (line = in.readLine()) != null; result = result + line) {
             }
         } catch (Exception var16) {
-            logger.info("发送 POST 请求出现异常！" + var16);
+            System.out.println("发送 POST 请求出现异常！" + var16);
             var16.printStackTrace();
         } finally {
             try {
